114c07016caa629e95b9cc03f95f4ab40e3f4253
Wen Zhang <iwenyou@gmail.com>
2017-01-15 14:22:38 -0800

    1st edition

---
+++ filter_small_numbers.rb
@@ -0,0 +1,22 @@
+# #Identify a skill
+# # =>loop through a collection of data to perform a compuation
+# #make a problem for that skill
+# # =>filter out any number > 5 in an array [4, 85, 5, 22, 1, 3, 2, 9]
+# #practice problems deliberately
+# 1. solve the problem
+# 2. reflection (where did I get stuck? What can i memorize?)
+
+greatThan5 = []
+
+[4, 85, 5, 22, 1, 3, 2, 9].each do | n|
+  
+  if n <= 5
+    greatThan5 << n
+  end  
+end
+
+p greatThan5
+
+# reflection
+# trouble knowing where to start, memorizing loop syntax
+
